62a64df605d86c6bbb9c0c0446be8a82c657ad55
David Coimbra <ds.coimbra96@gmail.com>
2018-04-26 22:56:41 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -3,9 +3,27 @@
 require_once("config.php");
 
 $root = new Usuario();
-
+$root->getList();
 $root->loadById(3);
-
 echo $root;
+
+echo "<br>";
+echo "<br>";
+
+$lista = Usuario::getList();
+echo json_encode($lista);
+
+echo "<br>";
+echo "<br>";
+
+$search = Usuario::search("jo");
+echo json_encode($search);
+
+echo "<br>";
+echo "<br>";
+
+$usuario = new Usuario();
+$usuario->login("root", "!@#$");
+echo $usuario;
 
  ?>
