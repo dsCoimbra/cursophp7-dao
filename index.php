dbefbfa708b501c9c35094570133b2fd60a4aa91
David Coimbra <ds.coimbra96@gmail.com>
2018-04-26 23:26:04 -0300

    metodos de insert e update

--- index.php
+++ index.php
@@ -7,23 +7,42 @@ $root->getList();
 $root->loadById(3);
 echo $root;
 
-echo "<br>";
+echo "<br>-------------------------------------------------";
 echo "<br>";
 
 $lista = Usuario::getList();
 echo json_encode($lista);
 
-echo "<br>";
+echo "<br>-------------------------------------------------";
 echo "<br>";
 
 $search = Usuario::search("jo");
 echo json_encode($search);
 
-echo "<br>";
+echo "<br>-------------------------------------------------";
 echo "<br>";
 
 $usuario = new Usuario();
 $usuario->login("root", "!@#$");
 echo $usuario;
+
+echo "<br>-------------------------------------------------";
+echo "<br>";
+
+$aluno = new Usuario("aluno", "@lun0");
+
+$aluno->insert();
+
+echo $aluno;
+
+echo "<br>-------------------------------------------------";
+echo "<br>";
+
+$usuario->loadById(8);
+
+$usuario->update("professor", "!@#$%¨&*");
+
+echo $usuario;
+
 
  ?>
